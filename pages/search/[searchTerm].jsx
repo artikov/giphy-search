8c8e2de55b1d80fd788ba818626612943d5b986d
Oybek Artikov <oybekartikov@icloud.com>
2022-12-10 20:00:00 +0500

    Add route for search

---
+++ pages/search/[searchTerm].jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+export default function Search() {
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <title>Search</title>
+        <link rel="stylesheet" href="/styles.css" />
+      </Head>
+      <h1>Search results for: {router.query.searchTerm}</h1>
+    </>
+  );
+}
